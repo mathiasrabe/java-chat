fe54998d77e434fd421dd34c6305c654dda8be0f
Mathias Rabe <mathias-rabe@post-x.de>
2011-04-24 13:02:48 +0200

    grafische oberfläche

--- src/client.java
+++ src/client.java
@@ -1,125 +1,142 @@
-import java.net.*;
-import java.util.*; //Bib für Eingabe über Konsole & Vector
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-//import java.util.Vector;
+import java.net.*;
+import java.util.*;
 import java.io.*;
 
-import com.google.api.detect.Detect;
-import com.google.api.translate.Language;
-import com.google.api.translate.Translate;
-
-public class client implements Runnable
+public class client extends JFrame
 {
 	public static final int PORT = 8765;
 	protected Socket socket;
-	protected Scanner in;
-	private volatile Thread connect;
-	private String consoleinput;
-	public String username;
-
+	protected BufferedReader in;
+	protected PrintStream out;
+	
+	JMenuBar menuBar;
+	JMenu menu;
+	JTextArea outputarea, userlistarea;
+	JTextField inputfield;
+	JButton sendButton;
 	
 	public client()
+ 	{
+		super("Java-Chat");
+ 		//setSize(300,300);
+ 		//setLocation(300,300);
+	   
+ 		// http://download.oracle.com/javase/tutorial/uiswing/layout/border.html
+ 		getContentPane().setLayout(new BorderLayout(1, 1));
+ 		
+ 		menuBar = new JMenuBar();
+ 		menu = new JMenu("A Menu");
+ 		menu.setMnemonic(KeyEvent.VK_A);
+ 		menu.getAccessibleContext().setAccessibleDescription(
+ 		        "The only menu in this program that has menu items");
+ 		menuBar.add(menu);
+ 		this.setJMenuBar(menuBar);
+	   
+ 		// http://download.oracle.com/javase/1.5.0/docs/api/javax/swing/JTextArea.html
+ 		outputarea = new JTextArea();
+ 		outputarea.setPreferredSize(new Dimension(500, 250));
+ 		outputarea.setEditable(false);
+ 		outputarea.setLineWrap(true);
+ 		outputarea.setWrapStyleWord(true);
+ 		outputarea.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+ 		JScrollPane scrollPane = new JScrollPane(outputarea);
+ 		getContentPane().add(scrollPane, BorderLayout.LINE_START); //FIXME
+ 		
+ 		userlistarea = new JTextArea();
+ 		userlistarea.setPreferredSize(new Dimension(150, 100));
+ 		userlistarea.setEditable(false);
+ 		getContentPane().add(userlistarea, BorderLayout.CENTER);
+ 		
+ 		JPanel panel = new JPanel();
+ 		panel.setLayout(new FlowLayout());
+	   
+ 		inputfield = new JTextField();
+ 		inputfield.setPreferredSize(new Dimension(600, 25));
+ 		panel.add(inputfield);
+ 		//getContentPane().add(inputfield);
+	   
+ 		Action sendText = new sendText();
+ 		sendButton = new JButton(sendText);
+ 		panel.add(sendButton);
+ 		getContentPane().add(panel, BorderLayout.PAGE_END);
+ 	}
+	
+	public void connect()
 	{
-		in = new Scanner(System.in);
-		System.out.println("Bitte geben Sie eine IP-Adresse ein:");
-		String input = in.nextLine();
-		// TODO Die Variable input muss auf Richtigkeit überprüft werden!
-		System.out.println("Bitte geben Sie Ihren Nickname ein:");
-		do {
-			if (username != null && username.contains(" "))
-				System.out.println("In Ihrem Benutzernamen darf kein Leerzeichen enthalten sein." +
-						           " Bitte geben Sie erneut ein:");
-			username = in.nextLine();
-		} while( username.isEmpty() || username.contains(" ") );
-		
 		try
 		{
-			socket = new Socket( input, PORT);
+			socket = new Socket( "localhost", PORT);
 		} catch (IOException e)
 		{
 			System.err.println("Verbindung fehlgeschlagen:"+ e);
 			System.exit(1);
 		}
 		
-		connect = new Thread(this);
-		connect.start();
-	}
-	
-	public void done()
-	{
-		// Zeugs um Thread zu stoppen
-		Thread moribund = connect;
-		connect = null;
-		moribund.interrupt();
-	}
-	
-	public void run()
-	{		
-		connection_client c = new connection_client(socket, this, username);
-		
-		// Zeugs um Thread zu stoppen
-		// siehe auch http://download.oracle.com/javase/1.5.0/docs/guide/misc/threadPrimitiveDeprecation.html
-		Thread thisThread = Thread.currentThread();
-		while (connect == thisThread) {
-			//Abfrage der Konsoleneingabe
-			consoleinput = in.nextLine();
-						
-			c.out.println( consoleinput );
-			
-			if(consoleinput.matches("/quit")) {
-				//alle Threads beenden
-				c.done();
-				this.done();
-			}
+		try
+		{
+			in = new BufferedReader(new InputStreamReader( socket.getInputStream() ));
+			out = new PrintStream(socket.getOutputStream());
+		} catch (IOException e)
+		{
+			try { socket.close(); } catch (IOException e2) {} ;
+			System.err.println("Fehler beim Erzeugen der Streams: " + e);
+			return;
 		}
-		System.out.println("bye bye ...");
 	}
 	
+	public void waitForServer()
+	{
+		String line;
+
+		try
+		{
+			while (true)
+			{
+				line=in.readLine();
+				if(line!=null)
+					outputarea.append(line + "\n");
+			}
+		} catch(IOException e)
+		{
+			if (!socket.isClosed())
+				System.out.println("Fehler: " + e);
+		}
+	}
+	
+	public void launchFrame()
+	{
+ 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
+ 		pack();
+ 		setVisible(true);
+	}
+
 	public static void main(String[] args)
 	{
-		new client();
+		client c = new client();
+		c.launchFrame();
+		c.connect();
+		c.waitForServer();
 	}
 	
-	/**
-	 * 
-	 * @param text der zu übersetzende Text
-	 * @return übersetzer Text, oder leerer String, wenn nicht erfolgreich
-	 */
-	private String translate(String text)
-	{
-		Translate.setHttpReferrer("moep.de"); // wozu?
-		Language lang = null;
-		String translatedText = new String();
+
+	public class sendText extends AbstractAction {
 		
-		// Spracherkennung
-		try {
-			lang = Detect.execute(text).getLanguage();
-		} catch (Exception e) {
-			System.err.println("Fehler bei Spracherkennung: " + e);
-			return translatedText;
-		}
-		
-		//Übersetze Text
-		try {
-			translatedText = Translate.execute(text, lang, Language.ENGLISH);
-			// FIXME Alles ins Englische übersetzen? bäää...
-		} catch (Exception e) {
-			System.err.println("Fehler bei Überstzung: " + e);
-		}
-		
-		return translatedText;
-		
-	}
-	
-	public void display(String line)
-	{
-		if ( !line.startsWith(username) ) {
-			// String newtext = translate(line);
-			//if ( !newtext.isEmpty() ) {
-			//	System.out.println(newtext);
-			//} else {
-				System.out.println(line);
-			//}
-		}
+	    public sendText()
+	    {
+	        super("Senden");
+	    }
+	    
+	    public void actionPerformed(ActionEvent e) {
+	    	String input = inputfield.getText();
+	    	if (input != null) {
+	    		out.println(input);
+	    		inputfield.setText("");
+	    	}
+	    }
 	}
 }
+ 
