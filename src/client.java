e36f84b5e21fef5ffbc9a71de4280d56a5245d7f
Mathias Rabe <mathias-rabe@post-x.de>
2011-04-25 20:43:16 +0200

    gui-client das erste mal benutzbar

--- src/client.java
+++ src/client.java
@@ -3,21 +3,21 @@ import java.awt.*;
 import java.awt.event.*;
 
 import java.net.*;
-import java.util.*;
 import java.io.*;
 
-public class client extends JFrame
+public class client extends JFrame implements KeyListener
 {
 	public static final int PORT = 8765;
 	protected Socket socket;
 	protected BufferedReader in;
 	protected PrintStream out;
 	
-	JMenuBar menuBar;
-	JMenu menu;
-	JTextArea outputarea, userlistarea;
-	JTextField inputfield;
-	JButton sendButton;
+	protected JMenuBar menuBar;
+	protected JMenu menu;
+	protected JScrollPane scrollPane;
+	protected JTextArea outputarea, userlistarea;
+	protected JTextField inputfield;
+	protected JButton sendButton;
 	
 	public client()
  	{
@@ -35,37 +35,45 @@ public class client extends JFrame
  		        "The only menu in this program that has menu items");
  		menuBar.add(menu);
  		this.setJMenuBar(menuBar);
+ 		
+ 		JPanel upperPanel = new JPanel();
+ 		upperPanel.setLayout(new BoxLayout(upperPanel, BoxLayout.X_AXIS));
 	   
  		// http://download.oracle.com/javase/1.5.0/docs/api/javax/swing/JTextArea.html
  		outputarea = new JTextArea();
- 		outputarea.setPreferredSize(new Dimension(500, 250));
  		outputarea.setEditable(false);
  		outputarea.setLineWrap(true);
  		outputarea.setWrapStyleWord(true);
  		outputarea.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
- 		JScrollPane scrollPane = new JScrollPane(outputarea);
- 		getContentPane().add(scrollPane, BorderLayout.LINE_START); //FIXME
+ 		scrollPane = new JScrollPane(outputarea);
+ 		scrollPane.setPreferredSize(new Dimension(500, 250));
+ 		scrollPane.setMinimumSize(new Dimension(100, 50));
+ 		upperPanel.add(scrollPane);
  		
  		userlistarea = new JTextArea();
- 		userlistarea.setPreferredSize(new Dimension(150, 100));
+ 		//userlistarea.setPreferredSize(new Dimension(150, 100));
+ 		userlistarea.setMinimumSize(new Dimension(50, 50));
  		userlistarea.setEditable(false);
- 		getContentPane().add(userlistarea, BorderLayout.CENTER);
+ 		upperPanel.add(userlistarea);
+ 		getContentPane().add(upperPanel, BorderLayout.CENTER);
  		
- 		JPanel panel = new JPanel();
- 		panel.setLayout(new FlowLayout());
+ 		
+ 		JPanel lowerPanel = new JPanel();
+ 		lowerPanel.setLayout(new BoxLayout(lowerPanel, BoxLayout.X_AXIS));
 	   
  		inputfield = new JTextField();
  		inputfield.setPreferredSize(new Dimension(600, 25));
- 		panel.add(inputfield);
+ 		inputfield.addKeyListener(this);
+ 		lowerPanel.add(inputfield);
  		//getContentPane().add(inputfield);
 	   
- 		Action sendText = new sendText();
- 		sendButton = new JButton(sendText);
- 		panel.add(sendButton);
- 		getContentPane().add(panel, BorderLayout.PAGE_END);
+ 		Action sendAction = new sendAction();
+ 		sendButton = new JButton(sendAction);
+ 		lowerPanel.add(sendButton);
+ 		getContentPane().add(lowerPanel, BorderLayout.PAGE_END);
  	}
 	
-	public void connect()
+	protected void connect()
 	{
 		try
 		{
@@ -88,7 +96,7 @@ public class client extends JFrame
 		}
 	}
 	
-	public void waitForServer()
+	protected void waitForServer()
 	{
 		String line;
 
@@ -97,8 +105,13 @@ public class client extends JFrame
 			while (true)
 			{
 				line=in.readLine();
-				if(line!=null)
-					outputarea.append(line + "\n");
+				if(line!=null) {
+					outputarea.setText( outputarea.getText() + line + "\n" ); //sollte dafür sorgen dass immer die unterste Zeile angezeigt wird
+					//outputarea.append(line + "\n");
+					outputarea.setCaretPosition(outputarea.getDocument().getLength());
+					//outputarea.setPreferredSize( outputarea.getPreferredScrollableViewportSize() );
+					scrollPane.revalidate();
+				}
 			}
 		} catch(IOException e)
 		{
@@ -107,7 +120,7 @@ public class client extends JFrame
 		}
 	}
 	
-	public void launchFrame()
+	protected void launchFrame()
 	{
  		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
  		pack();
@@ -122,10 +135,29 @@ public class client extends JFrame
 		c.waitForServer();
 	}
 	
+	private void sendText(String text) {
+		out.println(text);
+		inputfield.setText("");
+	}
 
-	public class sendText extends AbstractAction {
+	public void keyPressed(KeyEvent e) {			
+	}
+	
+	public void keyTyped(KeyEvent e) {
+	}
+	
+	// wenn Enter gedrückt wird, wird das Zeug weg geschickt
+	public void keyReleased(KeyEvent e) {
+		String input = inputfield.getText();
+		int key = e.getKeyCode();
+		if(key == KeyEvent.VK_ENTER && input != null) {
+			sendText(input);
+		}
+	}
+	
+	private class sendAction extends AbstractAction {
 		
-	    public sendText()
+	    public sendAction()
 	    {
 	        super("Senden");
 	    }
@@ -133,8 +165,7 @@ public class client extends JFrame
 	    public void actionPerformed(ActionEvent e) {
 	    	String input = inputfield.getText();
 	    	if (input != null) {
-	    		out.println(input);
-	    		inputfield.setText("");
+	    		sendText(input);
 	    	}
 	    }
 	}
